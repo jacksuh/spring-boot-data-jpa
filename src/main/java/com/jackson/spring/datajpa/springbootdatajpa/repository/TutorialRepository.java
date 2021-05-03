578947d5202f4189431f777184f571101f984412
Jackson Santos <56548381+jacksuh@users.noreply.github.com>
2021-05-03 12:03:37 -0300

    Create model and controller

---
+++ src/main/java/com/jackson/spring/datajpa/springbootdatajpa/repository/TutorialRepository.java
@@ -0,0 +1,13 @@
+package com.jackson.spring.datajpa.springbootdatajpa.repository;
+
+
+import com.jackson.spring.datajpa.springbootdatajpa.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+    List<Tutorial> findByPublished(boolean published);
+    List<Tutorial> findByTitleContaining(String title);
+
+}
